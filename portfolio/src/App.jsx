a21b12ea5983586db76982cf6e6fefa23f4f53d4
Elijah Amian <eramian125@gmail.com>
2022-02-03 11:21:08 -0800

    Worked On Menu

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,13 +3,18 @@ import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
-import Works from "./components/works/Works";
+import Works from "./components/works/Education";
 import "./App.scss";
+import { useState } from "react";
+import Menu from "./components/menu/Menu";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
-      <Topbar/>
+
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- portfolio/src/components/topbar/Topbar.jsx
+++ portfolio/src/components/topbar/Topbar.jsx
@@ -1,23 +1,28 @@
 import "./topbar.scss";
 import {Person, Mail} from "@material-ui/icons";
 
-export default function Topbar() {
-  return <div className="topbar">
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">
-            genius.
-          </a>
-          <Person></Person>
+            <a href="#intro" className="logo">
+              genius.
+            </a>
           <div className="itemContainer">
-          
+            <Person className="icon"></Person>
+            <span>+1 808 393 9687</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon"></Mail>
+            <span>elijahamian@outlook.com</span>
           </div>
         </div>
-        <div>
-          Hello
-        </div>
         <div className="right">
-          this is right
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
         </div>
       </div>
   </div>;
