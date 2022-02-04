90e8172c9d68a623f420f110f003ff673434a00e
Elijah Amian <eramian125@gmail.com>
2022-02-04 10:26:39 -0800

    Worked on Intro page

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -2,6 +2,20 @@ import "./intro.scss";
 
 export default function Intro() {
   return <div className="intro" id="intro">
-
+    <div className="left">
+      <div className="imgContainer">
+        <img src="assets/man.png"></img>
+      </div>
+    </div>
+    <div className="right">
+      <div className="wrapper">
+        <h2>Hi There, I'm</h2>
+        <h1>Elijah Amian</h1>
+        <h3><span>Third Year Computer Science Major <br></br>at University of Washington</span></h3>
+      </div>
+      <a href="#portfolio">
+        <img src="assets/down.png" alt=""></img>
+      </a>
+    </div>
   </div>;
 }
