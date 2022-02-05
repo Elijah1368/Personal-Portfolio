00ae55c51a45ad199b6ef355c65ff68bec668352
Elijah Amian <eramian125@gmail.com>
2022-02-04 16:29:52 -0800

    Worked on intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,20 @@
 import "./intro.scss";
+import {init} from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+  const textRef = useRef();
+  
+  useEffect(()=>{
+    init(textRef.current, { 
+      showCursor: true, 
+      typeSpeed: 40, backSpeed: 20,
+      backDelay: 1500,
+      strings: ['Third Year Computer Science Major at University of Washington', 'Self-taught Web Developer' ] });
+  },[]);
+
   return <div className="intro" id="intro">
-    <div className="left">
+    <div className="left"> 
       <div className="imgContainer">
         <img src="assets/man.png"></img>
       </div>
@@ -11,7 +23,7 @@ export default function Intro() {
       <div className="wrapper">
         <h2>Hi There, I'm</h2>
         <h1>Elijah Amian</h1>
-        <h3><span>Third Year Computer Science Major <br></br>at University of Washington</span></h3>
+        <h3><span ref={textRef}></span></h3>
       </div>
       <a href="#portfolio">
         <img src="assets/down.png" alt=""></img>
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,17 @@ import "./portfolio.scss";
 
 export default function Portfolio() {
   return <div className="portfolio" id="portfolio">
-
+    <h1>Portfolio</h1>
+    <ul>
+      <li>Featured</li>
+      <li>Web App</li>
+      <li>Design</li>
+    </ul>
+    <div className="container">
+      <div className="item">
+        <img src=""></img>
+        <h3></h3>
+      </div>
+    </div>
   </div>;
 }
