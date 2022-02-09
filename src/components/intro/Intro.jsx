f079a4c2e2a56ead40763c1ab380e99ea4d007f9
Elijah Amian <eramian125@gmail.com>
2022-02-09 15:10:58 -0800

    Worked on Portfolio Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -21,9 +21,13 @@ export default function Intro() {
     </div>
     <div className="right">
       <div className="wrapper">
-        <h2>Hi There, I'm</h2>
-        <h1>Elijah Amian</h1>
-        <h3><span ref={textRef}></span></h3>
+  
+          <h2>Hi There, I'm</h2>
+          <h1>Elijah Amian</h1>
+     
+       
+          <h3><span ref={textRef}></span></h3>
+       
       </div>
       <a href="#portfolio">
         <img src="assets/down.png" alt=""></img>
